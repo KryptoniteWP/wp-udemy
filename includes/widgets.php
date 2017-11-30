05a06990254a71e5cb18864ecdae28d380f4f62e
flowdee <ddjjmm@gmx.de>
2017-11-30 11:55:07 +0100

    removed widget_text filter resolves #14

--- includes/widgets.php
+++ includes/widgets.php
@@ -15,7 +15,7 @@ if( !defined( 'ABSPATH' ) ) exit;
 include_once UFWP_DIR . 'includes/widgets/class.widget.courses.php';
 include_once UFWP_DIR . 'includes/widgets/class.widget.search.php';
 
-/*
+/**
  * Register Widgets
  */
 function ufwp_register_widgets() {
@@ -24,8 +24,10 @@ function ufwp_register_widgets() {
 }
 add_action( 'widgets_init', 'ufwp_register_widgets' );
 
-/*
- * Build shortcode
+/**
+ * Build our widget shortcode
+ *
+ * @param array $atts
  */
 function ufwp_widget_do_shortcode( $atts = array() ) {
 
@@ -48,7 +50,7 @@ function ufwp_widget_do_shortcode( $atts = array() ) {
     }
 }
 
-/*
+/**
  * Execute shortcodes within text widgets
  */
 $options = ufwp_get_options();
@@ -56,38 +58,3 @@ $options = ufwp_get_options();
 if ( isset ( $options['widget_text_shortcodes'] ) ) {
     add_filter( 'widget_text', 'do_shortcode');
 }
-
-/*
- * Handle shortcode in text widgets
- */
-function ufwp_widget_text( $widget_text, $instance, $widget ) {
-
-    static $text_widget_scripts_loaded = false;
-
-    if ( has_shortcode( $instance['text'], 'ufwp' ) || has_shortcode( $instance['text'], 'udemy' ) ) {
-
-        // Add widget template if missing
-        if ( strpos( $instance['text'], 'template') === false ) {
-            $widget_text = str_replace( '[ufwp', '[ufwp template="widget"', $widget_text );
-
-        // Reset invalid templates
-        } elseif ( strpos( $instance['text'], 'template="standard"') !== false ) {
-            $widget_text = str_replace( 'template="standard"', 'template="widget"', $widget_text );
-
-        } elseif ( strpos( $instance['text'], 'template="grid"') !== false ) {
-            $widget_text = str_replace( 'template="grid"', 'template="widget"', $widget_text );
-
-        } elseif ( strpos( $instance['text'], 'template="list"') !== false ) {
-            $widget_text = str_replace( 'template="list"', 'template="widget"', $widget_text );
-        }
-
-        // Load scripts
-        if ( ! $text_widget_scripts_loaded ) {
-            ufwp_load_scripts();
-            $text_widget_scripts_loaded = true;
-        }
-    }
-
-    return $widget_text;
-}
-add_filter( 'widget_text', 'ufwp_widget_text', 1, 3 );
